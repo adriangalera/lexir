58aa6242619278521a86517bfaa946ed6b8e4453
Adrian Galera <adrian.galera.87@gmail.com>
2025-05-15 10:59:11 +0200

    Move exploratory scripts into its own folder

---
+++ dictionaries/scripts/explore.py
@@ -0,0 +1,19 @@
+import json
+import urllib.parse
+
+filename = "../kaikki-spanish.jsonl"
+line_limit = 10000
+count = 0
+with open(filename, encoding="utf-8") as f:
+    for line in f:
+        data = json.loads(line)
+        word = data["word"]
+        pos_title = data.get("pos_title", "N/A")
+        senses = data["senses"]
+        sense_length = len(senses)
+        sanitized_word = urllib.parse.quote(word)
+        link = f"https://es.wiktionary.org/wiki/{sanitized_word}"
+        print(f"{word}: {pos_title} {sense_length} significados. Link: {link}")
+        count = count + 1
+        if count == line_limit:
+            exit(0)
